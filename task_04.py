e42b4a06fb7f14e43e6a5ddd4737ce6f75292b9f
DmitryTaushev <123morfix@gmail.com>
2024-11-17 21:41:59 +0500

    Выполнили таск 4

--- task_04.py
+++ task_04.py
@@ -7,3 +7,22 @@
 # удалить какой-то из жанров по вашему выбору;
 # удалить некий случайный жанр;
 # преобразовать множество обратно в список.
+
+cinema_genres = ["комедия", "экшн", "пеплум", "триллер", "комедия","пеплум"]
+# преобразовать данный список в множество;
+cinema_genres_set = set(cinema_genres)
+# добавить 2 жанра на ваш выбор (то что вы захотите);
+cinema_genres_set.add("боевик")
+print(cinema_genres_set)
+# удалить какой-то из жанров по вашему выбору;
+cinema_genres_set.remove("комедия")
+print(cinema_genres_set)
+# удалить некий случайный жанр;
+cinema_genres_set.pop()
+print(cinema_genres_set)
+# преобразовать множество обратно в список.
+cinema_genres_list = list(cinema_genres_set)
+print(cinema_genres_list)
+
+
+
