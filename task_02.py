eb40feebd45c6251c0d5ff0e9362d5df478d751d
DmitryTaushev <123morfix@gmail.com>
2024-11-17 16:37:56 +0500

    Выполнили таск 2

--- task_02.py
+++ task_02.py
@@ -10,3 +10,15 @@
 # Вы можете преобразовывать кортежи в другой тип данных или
 # записывать их в новые переменные. Результат работы программы
 # должен выглядеть следующим образом:
+
+cinema_genres = ("Комедия", "Экшн", "Пеплум", "Триллер")
+cinema_genres_list = list(cinema_genres)
+cinema_genres_list[1] = 'Боевик'
+cinema_genres = tuple(cinema_genres_list)
+print(cinema_genres)
+
+cinema_genres_list.insert(2,'Мелодрама')
+cinema_genres = tuple(cinema_genres_list)
+print(cinema_genres)
+print(','.join(cinema_genres))
+
